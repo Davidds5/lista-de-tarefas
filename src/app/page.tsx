50ecea2d6e21903fd9cf631a61dcce6208c44a35
Davidds5 <fudeoagr12@gmail.com>
2026-07-05 21:16:38 -0300

    feat: implement task list interface and add reusable Alert Dialog component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import { Plus, List, Check, X, SquarePen, Trash } from 'lucide-react';
+import { Plus, List, Check, X, SquarePen, Trash, ListCheck, Sigma } from 'lucide-react';
 
 
 const Home = () => {
@@ -17,42 +18,65 @@ const Home = () => {
         </div>
         <Separator />
 
+        { /**tags de finalizados pendentes e concluidos*/}
         <div className="flex py-2 gap-2">
-          <Badge className="cursor-pointer"><List />Todas</Badge>
-          <Badge className="cursor-pointer"><X />Pendentes</Badge>
-          <Badge className="cursor-pointer"><Check />Concluídas</Badge>
+          <Badge className="cursor-pointer" variant="default"><List />Todas</Badge>
+          <Badge className="cursor-pointer" variant={"outline"}><X />Pendentes</Badge>
+          <Badge className="cursor-pointer" variant={"outline"}><Check />Concluídas</Badge>
         </div>
 
-        <div className="py-2">
-
-          <div className="h-10 flex justify-between items-center border-b-1 border-t-1">
+        {/**Lista de tarefas */}
+        <div className="  border-b-1 border-t-1">
+          <div className="h-10 flex justify-between items-center border-t-1 ">
             <div className="w-1 h-full bg-green-300"></div>
             <p className=" text-sm flex-1 px-2">Teste de mesa</p>
             <div className="flex items-center gap-1">
-              <SquarePen size={20} className="cursor-pointer" />
-              <Trash size={20} className="cursor-pointer" />
-            </div>
-          </div>
-
-          <div className="h-10 flex justify-between items-center border-b-1 border-t-1">
-            <div className="w-1 h-full bg-green-300"></div>
-            <p className=" text-sm flex-1 px-2">Teste de mesa</p>
-            <div className="flex items-center gap-1">
-              <SquarePen size={20} className="cursor-pointer" />
-              <Trash size={20} className="cursor-pointer" />
-            </div>
-          </div>
-
-          <div className="h-10 flex justify-between items-center border-b-1 border-t-1">
-            <div className="w-1 h-full bg-green-300"></div>
-            <p className=" text-sm flex-1 px-2">Teste de mesa</p>
-            <div className="flex items-center gap-1">
-              <SquarePen size={20} className="cursor-pointer" />
-              <Trash size={20} className="cursor-pointer" />
+              <SquarePen size={16} className="cursor-pointer" />
+              <Trash size={16} className="cursor-pointer" />
             </div>
           </div>
         </div>
 
+        { /**Area tarefas concluidas, Limpar tarefas  */}
+        <div className=" flex justify-between items-center mt-4">
+          <div className="flex gap-1 items-center">
+            <ListCheck size={16} />
+            <p className="text-xs">tarefas concluidas (1/1)</p>
+          </div>
+          <Button className="text-xs h-6 cursor-pointer"><Trash />
+            Limpar tarefas concluidas
+          </Button>
+        </div>
+
+        { /** Barra de porcentagem*/}
+        <div className="w-full h-2 bg-gray-200 rounded-md mt-2">
+          <div className="h-full bg-blue-500 rounded-md" style={{ width: "50%" }}></div>
+        </div>
+
+        { /**Tarefas no total */}
+        <div className="flex mt-2 justify-end items-center gap-1">
+          <Sigma size={16} />
+          <p className="text-xs">3 Tarefas no Total</p>
+        </div>
+
+
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button className="text-xs h-7 cursor-pointer" variant={"outline"}>Limpar tarefas concluidas</Button>
+          </AlertDialogTrigger>
+
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Tem certeza que deseja excluir x itens ?</AlertDialogTitle>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel className="cursor-pointer">Cancelar</AlertDialogCancel>
+              <AlertDialogAction className="cursor-pointer">Sim, excluir</AlertDialogAction>
+            </AlertDialogFooter>
+
+
+          </AlertDialogContent>
+        </AlertDialog>
 
 
 
